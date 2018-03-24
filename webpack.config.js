31e98930d2c5b07a391d101ca6040bf4dca8d98d
Przemek Mazur <Przemek1713@gmail.com>
2018-03-24 01:29:24 +0100

    var env added

--- webpack.config.js
+++ webpack.config.js
@@ -30,15 +30,23 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
+    var env = process.env.NODE_ENV || 'development';
+    var plugins = [
+    new HtmlWebpackPlugin({
             template: 'src/index.html',
             filename: 'index.html',
-            inject: 'body'
-        }),
+            inject: 'body',
+        })
+    ];
+
+    console.log('NODE_ENV:', env);
+
+    if (env === 'production') {
+    plugins.push(
         new webpack.optimize.UglifyJsPlugin(),
         new OptimizeJsPlugin({
           sourceMap: false
         })
-    ]    
+      );
+    }   
 };
